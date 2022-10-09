f0e382cb73210f2e1c96d884b03c242a411beb07
Devin Slate <home@Devins-MBP.hsd1.co.comcast.net>
2022-10-09 14:02:15 -0600

    explicit set encrypton s3_bucket_keys false

--- main.tf
+++ main.tf
@@ -34,8 +34,8 @@ resource "aws_s3_bucket_public_access_block" "default" {
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   bucket = aws_s3_bucket.default.bucket
   rule {
+    bucket_key_enabled = false
     apply_server_side_encryption_by_default {
-      # kms_master_key_id = aws_kms_key.mykey.arn
       sse_algorithm = "AES256"
     }
   }
